82f5cf022b0d14adbcb4838864541dda16047162
rgblotus <48052725+rgblotus@users.noreply.github.com>
2022-05-04 09:55:00 +0530

    Delete cnp.js

--- cnp.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  var myFlipster = $("#carousel").flipster({
-    style: "carousel",
-    spacing: -0.2,
-    buttons: true,
-    loop: true,
-    autoplay: true | 5000,
-  });
-});
