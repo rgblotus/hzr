e49722e9733a210000ab634a288269af97b69812
rgblotus <48052725+rgblotus@users.noreply.github.com>
2022-05-04 09:57:02 +0530

    Add files via upload

---
+++ static/js/cnp.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  var myFlipster = $("#carousel").flipster({
+    style: "carousel",
+    spacing: -0.2,
+    buttons: true,
+    loop: true,
+    autoplay: true | 5000,
+  });
+});
